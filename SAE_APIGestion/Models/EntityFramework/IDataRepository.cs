fdb8d3c94801e34a8e30d2ce46118d0618940323
taillezb <taillezb@D101-S30>
2024-11-08 11:07:15 +0100

    IData

---
+++ SAE_APIGestion/Models/EntityFramework/IDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SAE_APIGestion.Models.EntityFramework
+{
+    public interface IDataRepository<TEntity>
+    {
+        Task<ActionResult<IEnumerable<TEntity>>> GetAllAsync();
+        Task<ActionResult<TEntity>> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+        Task DeleteAsync(TEntity entity);
+
+
+    }
+}

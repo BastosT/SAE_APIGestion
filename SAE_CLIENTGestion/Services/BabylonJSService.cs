e39ca18c5c50ebbe3d8e187d64b5bdda6e5d3866
gallottm <gallottm@D101-S29>
2024-11-19 09:40:35 +0100

    Fix render salles

--- SAE_CLIENTGestion/Services/BabylonJSService.cs
+++ SAE_CLIENTGestion/Services/BabylonJSService.cs
@@ -10,7 +10,6 @@ public class BabylonJSService : IBabylonJSService
 {
     private readonly IJSRuntime _jsRuntime;
     private readonly RoomData _roomData;
-
     public BabylonJSService(IJSRuntime jsRuntime)
     {
         _jsRuntime = jsRuntime;
@@ -22,13 +21,13 @@ public class BabylonJSService : IBabylonJSService
                 Hauteur = 270,
                 Equipements = new List<Equipment>
                 {
-                    new Equipment { Nom = "Fenetre 1", Largeur = 100, Hauteur = 165, PositionX = 6, PositionY = 3 },
-                    new Equipment { Nom = "Vitre 1", Largeur = 89, Hauteur = 161, PositionX = 125, PositionY = 6 },
-                    new Equipment { Nom = "Vitre 2", Largeur = 89, Hauteur = 161, PositionX = 237, PositionY = 6 },
-                    new Equipment { Nom = "Fenetre 2", Largeur = 100, Hauteur = 165, PositionX = 345, PositionY = 3 },
-                    new Equipment { Nom = "Vitre 3", Largeur = 89, Hauteur = 161, PositionX = 482, PositionY = 6 },
-                    new Equipment { Nom = "Radiateur 1", Largeur = 160, Hauteur = 80, PositionX = 34, PositionY = 180 },
-                    new Equipment { Nom = "Radiateur 2", Largeur = 160, Hauteur = 80, PositionX = 256, PositionY = 180 }
+                    new Equipment { Nom = "Fenetre 1", Type = EquipmentType.Window, Largeur = 100, Hauteur = 165, PositionX = 6, PositionY = 3 },
+                    new Equipment { Nom = "Vitre 1", Type = EquipmentType.DoorWindow, Largeur = 89, Hauteur = 161, PositionX = 125, PositionY = 6 },
+                    new Equipment { Nom = "Vitre 2", Type = EquipmentType.DoorWindow, Largeur = 89, Hauteur = 161, PositionX = 237, PositionY = 6 },
+                    new Equipment { Nom = "Fenetre 2", Type = EquipmentType.Window, Largeur = 100, Hauteur = 165, PositionX = 345, PositionY = 3 },
+                    new Equipment { Nom = "Vitre 3", Type = EquipmentType.DoorWindow, Largeur = 89, Hauteur = 161, PositionX = 482, PositionY = 6 },
+                    new Equipment { Nom = "Radiateur 1", Type = EquipmentType.Radiator, Largeur = 160, Hauteur = 80, PositionX = 34, PositionY = 180 },
+                    new Equipment { Nom = "Radiateur 2", Type = EquipmentType.Radiator, Largeur = 160, Hauteur = 80, PositionX = 256, PositionY = 180 }
                 }
             },
             MurEntree = new Wall
@@ -37,7 +36,7 @@ public class BabylonJSService : IBabylonJSService
                 Hauteur = 270,
                 Equipements = new List<Equipment>
                 {
-                    new Equipment { Nom = "Porte", Largeur = 93, Hauteur = 205, PositionX = 55, PositionY = 67 },
+                    new Equipment { Nom = "Porte", Type = EquipmentType.Door, Largeur = 93, Hauteur = 205, PositionX = 55, PositionY = 67 },
                 }
             },
             MurDroite = new Wall
@@ -46,8 +45,8 @@ public class BabylonJSService : IBabylonJSService
                 Hauteur = 270,
                 Equipements = new List<Equipment>
                 {
-                    new Equipment { Nom = "Capteur 1", Largeur = 10, Hauteur = 10, PositionX = 178, PositionY = 98 },
-                    new Equipment { Nom = "Capteur 2", Largeur = 5, Hauteur = 5, PositionX = 585, PositionY = 161 }
+                    new Equipment { Nom = "Capteur 1", Type = EquipmentType.Sensor, Largeur = 15, Hauteur = 15, PositionX = 178, PositionY = 98 },
+                    new Equipment { Nom = "Capteur 2", Type = EquipmentType.Sensor, Largeur = 10, Hauteur = 10, PositionX = 585, PositionY = 161 }
                 }
             },
             MurGauche = new Wall
@@ -56,8 +55,8 @@ public class BabylonJSService : IBabylonJSService
                 Hauteur = 270,
                 Equipements = new List<Equipment>
                 {
-                    new Equipment { Nom = "Capteur 1", Largeur = 5, Hauteur = 5, PositionX = 316, PositionY = 70 },
-                    new Equipment { Nom = "Capteur 2", Largeur = 5, Hauteur = 5, PositionX = 662, PositionY = 156 }
+                    new Equipment { Nom = "Capteur 1", Type = EquipmentType.Sensor, Largeur = 10, Hauteur = 10, PositionX = 316, PositionY = 70 },
+                    new Equipment { Nom = "Capteur 2", Type = EquipmentType.Sensor, Largeur = 10, Hauteur = 10, PositionX = 662, PositionY = 156 }
                 }
             },
         };
@@ -92,8 +91,19 @@ public class Wall
 public class Equipment
 {
     public string Nom { get; set; }
+    public EquipmentType Type { get; set; }
     public int Largeur { get; set; }
     public int Hauteur { get; set; }
     public int PositionX { get; set; }
     public int PositionY { get; set; }
+}
+
+public enum EquipmentType
+{
+    Window,     // Fenêtre
+    DoorWindow, // Vitre
+    Door,       // Porte
+    Radiator,   // Radiateur
+    Sensor,     // Capteur
+    Other       // Autre équipement non spécifié
 }

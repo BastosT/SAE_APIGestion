9b3eec378ba4c1dda4682675848671c8ccf391aa
Geek <geekgames74.fr@gmail.com>
2024-11-22 11:03:50 +0100

    front viewmodels

---
+++ SAE_CLIENTGestion/Services/IService.cs
@@ -0,0 +1,13 @@
+﻿namespace SAE_CLIENTGestion.Services
+{
+    public interface IService<TEntity>
+    {
+
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(int id);
+        Task PostAsync(TEntity entity);
+        Task PutAsync(int id, TEntity entity);
+        Task DeleteAsync(int id);
+
+    }
+}
---
+++ SAE_CLIENTGestion/Services/WSServiceEquipement.cs
@@ -0,0 +1,32 @@
+﻿using SAE_CLIENTGestion.Models;
+
+namespace SAE_CLIENTGestion.Services
+{
+    public class WSServiceEquipement : IService<Equipement>
+    {
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Equipement>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Equipement?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostAsync(Equipement entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PutAsync(int id, Equipement entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

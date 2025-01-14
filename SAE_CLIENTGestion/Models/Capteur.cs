4d7d6a8eaa10f2c63d10d111d73db422bb831cc6
gallottm <gallottm@A275-S24>
2025-01-14 09:34:25 +0100

    e

--- SAE_CLIENTGestion/Models/Capteur.cs
+++ SAE_CLIENTGestion/Models/Capteur.cs
@@ -6,13 +6,13 @@ namespace SAE_CLIENTGestion.Models
         public int CapteurId { get; set; }
         public string Nom { get; set; }
         public bool EstActif { get; set; }
-        public double? DistanceFenetre { get; set; }
-        public double? Longueur { get; set; }
-        public double? Hauteur { get; set; }
-        public double PositionX { get; set; }
-        public double PositionY { get; set; }
-        public double? DistancePorte { get; set; }
-        public double? DistanceChauffage { get; set; }
+        public double? Longueur { get; set; } = 0;
+        public double? Hauteur { get; set; } = 0;
+        public double PositionX { get; set; } = 0;
+        public double PositionY { get; set; } = 0;
+        public double? DistanceFenetre { get; set; } = 0;
+        public double? DistancePorte { get; set; } = 0;
+        public double? DistanceChauffage { get; set; } = 0;
         public int? SalleId { get; set; }
         public int? MurId { get; set; }
         public virtual Mur Mur { get; set; }
